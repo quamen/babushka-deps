22283048c703fc80d81207949d1db82a24b158cb
Gareth Townsend <gareth.townsend@me.com>
2010-10-25 14:45:10 +1100

    Hudson deps

---
+++ hudson.rb
@@ -0,0 +1,19 @@
+dep 'hudson' do
+
+  requires 'java'
+
+  met? do
+    result = shell('dpkg -s hudson')
+    result && result['Status: install ok installed']
+  end
+
+  meet do
+    shell('wget -O /tmp/hudson-apt-key http://hudson-ci.org/debian/hudson-ci.org.key')
+    sudo('apt-key add /tmp/hudson-apt-key')
+    shell('rm /tmp/hudson-apt-key')
+    shell('wget -O /tmp/hudson.dep http://hudson-ci.org/latest/debian/hudson.deb')
+    sudo('dpkg --install /tmp/hudson.dep')
+    shell('rm /tmp/hudson.dep')
+  end
+
+end
