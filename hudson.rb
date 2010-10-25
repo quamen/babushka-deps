e06d8ed682d5cff367824c1c3421f4fd4dbbe940
Gareth Townsend <gareth.townsend@me.com>
2010-10-26 08:41:40 +1100

    and hudson relies on it

--- hudson.rb
+++ hudson.rb
@@ -1,6 +1,6 @@
 dep 'hudson' do
 
-  requires 'java-virtual-machine.managed', 'daemon.managed'
+  requires 'gcj-jre-headless.managed', 'daemon.managed'
 
   met? do
     result = shell('dpkg -s hudson')
