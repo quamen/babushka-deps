346a1a36a54e0111a578d86582bcfe37d7962125
Gareth Townsend <gareth.townsend@me.com>
2010-10-26 09:44:36 +1100

    more tweaks

--- java.rb
+++ java.rb
@@ -2,15 +2,7 @@ dep "default-jre.managed" do
   provides ["java"]
 end
 
-dep "gcj-jdk" do
+dep "gcj-jdk.managed" do
   requires 'default-jre.managed'
-  
-  met? do
-    result = shell('dpkg -s gcj-jdk')
-    result && result['Status: install ok installed']
-  end
-  
-  meet do
-    sudo('apt-get install gcj-jdk')
-  end
+  provides []
 end
