41323fabc70d97a5958123958b378e08199dc977
Gareth Townsend <gareth.townsend@me.com>
2011-04-03 18:16:25 +1000

    Re-work the zsh install"

--- star.rb
+++ star.rb
@@ -14,6 +14,31 @@ dep 'osx essential applications' do
 end
 
 dep 'development tools' do
-  requires 'TextMate.app',
-           'homebrew'
+  requires 'dot-files',
+           'TextMate.app',
+           'homebrew',
+           'zsh'
+end
+
+dep 'zsh' do
+  requires 'wget.managed'
+  requires 'zsh.managed'
+  
+  met? { "~/.oh-my-zsh".p.exists? }
+  meet { login_shell('wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh') }
+end
+
+dep 'dot-files' do
+  requires 'git'
+  
+  met? { ".ackrc".p.exists? }
+  meet {
+    shell('mkdir -p ~/Code/github/quamen')
+    in_dir('~/Code/github/quamen') do
+      shell('git clone git@github.com:quamen/dot-files.git')
+    end
+    in_dir('~/Code/github/quamen/dot-files') do
+      shell('./clone_and_link.sh')
+    end
+  }
 end
