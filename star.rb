83cd9a5398fdf6462b379eb798f70c23592526b9
Gareth Townsend <gareth.townsend@me.com>
2011-04-03 14:07:34 +1000

    Install the essential OS X applications

--- star.rb
+++ star.rb
@@ -1,3 +1,11 @@
 dep '*' do
+  requires 'osx essential applications'
+end
 
+dep 'osx essential applications' do
+  requires '1Password.app',
+           'Dropbox.app',
+           'Google Chrome.app',
+           'LaunchBar.app',
+           'SizeUp.app'
 end
