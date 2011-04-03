ae05bad27cc2d66650498821014355806ffd95d8
Gareth Townsend <gareth.townsend@me.com>
2011-04-03 14:14:17 +1000

    OS X Application dependencies

---
+++ osx_applications.rb
@@ -0,0 +1,23 @@
+dep '1Password.app' do
+  source 'http://aws.cachefly.net/aws/dmg/1PW3/English/1Password-3.2.5.dmg'
+end
+
+dep 'Dropbox.app' do
+  source 'http://cdn.dropbox.com/Dropbox%201.0.10.dmg'
+end
+
+dep 'Caffeine' do
+  source 'http://download.lightheadsw.com/download.php?software=caffeine'
+end
+
+dep 'Google Chrome.app' do
+  source 'http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg'
+end
+
+dep 'LaunchBar.app' do
+  source 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.0.3.dmg'
+end
+
+dep 'SizeUp.app' do
+  source 'http://irradiatedsoftware.com/download/SizeUp.zip'
+end
