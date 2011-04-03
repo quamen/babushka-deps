548ce219dc78efb4cdb138ffb5f0117dd7039169
Gareth Townsend <gareth.townsend@me.com>
2011-04-03 21:12:26 +1000

    re-write dot-files and zsh setup

--- star.rb
+++ star.rb
@@ -14,15 +14,21 @@ dep 'osx essential applications' do
 end
 
 dep 'development tools' do
-  requires 'dot-files',
-           'TextMate.app',
+  requires 'TextMate.app',
            'homebrew',
-           'zsh'
+           'oh-my-zsh',
+           'dot-files'
 end
 
 dep 'zsh' do
-  requires 'wget.managed'
   requires 'zsh.managed'
+  met? { sudo('echo \$SHELL', :as => var(:username), :su => true) == which('zsh') }
+  meet { sudo("chsh -s '#{which('zsh')}' #{var(:username)}") }
+end
+
+dep 'oh-my-zsh' do
+  requires 'wget.managed'
+  requires 'zsh'
   
   met? { "~/.oh-my-zsh".p.exists? }
   meet { login_shell('wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh') }
@@ -33,10 +39,8 @@ dep 'dot-files' do
   
   met? { "~/Code/github/quamen/dot-files".p.exists? }
   meet {
-    shell('mkdir -p ~/Code/github/quamen')
-    in_dir('~/Code/github/quamen') do
-      shell('git clone git@github.com:quamen/dot-files.git')
-    end
+    login_shell('mkdir -p ~/Code/github/quamen') unless ' ~/Code/github/quamen'.p.exists?
+    git 'https://github.com/quamen/dot-files.git', :to => '~/Code/github/quamen/dot-files'
     in_dir('~/Code/github/quamen/dot-files') do
       shell('./clone_and_link.sh')
     end
